f14296ff0ea25cf519b83f1728d65daf9c87759a
minsang95 <154496356+minsang95@users.noreply.github.com>
2024-02-02 02:35:10 +0900

    Feat - 인벤토리 UI 미완

---
+++ Assets/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+}
